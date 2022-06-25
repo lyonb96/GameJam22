2b81c553afa644bfbc85cd33d3cd9ff05e86a491
Brendan Lyon <brendan.lyon@claritymis.com>
2022-06-24 21:38:00 -0500

    Ship movement refinement

--- scripts/Ship.cs
+++ scripts/Ship.cs
@@ -6,44 +6,108 @@ public class Ship : Node2D
 	// Stats
 	public float MoveSpeed { get; set; }
 
-	// Velocity
-	public Vector2 Velocity { get; set; }
+	// Max rotation speed
+	public float RotationSpeed { get; set; }
+
+	// Current Velocity
+	public Vector2 LocalVelocity { get; set; }
+
+	// Rotation Rate
 	public float SlowRate { get; set; }	//Number from 0 - 1 which increases slow with a larger number
+
+	public bool BuildMode { get; set; }
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Velocity = Vector2.Zero;
+		LocalVelocity = Vector2.Zero;
 
 		//Variable Initialization
-		MoveSpeed = 50F;
+		MoveSpeed = 75F;
+		RotationSpeed = 10.0F;
 		SlowRate = 2F;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		var hasInput = false;
+		// Handle Build Mode
+		if (Input.IsActionJustPressed("SwitchMode"))
+		{
+			BuildMode = !BuildMode;
+		}
+		// Handle angular velocity
+		if (!BuildMode)
+		{
+			var localMouse = GetLocalMousePosition();
+			var dirToMouse = localMouse.Normalized();
+			var mouseRotation = Mathf.Rad2Deg(dirToMouse.Angle());
+			RotationDegrees += SmallestMagnitude(mouseRotation, mouseRotation * RotationSpeed * delta);
+		}
+		// Handle linear movement
+		var hasLinearMovement = false;
 		if (Input.IsActionPressed("MoveForward"))
 		{
-			Velocity += new Vector2(delta * MoveSpeed, 0);
-			hasInput = true;
+			LocalVelocity += new Vector2(delta * MoveSpeed, 0);
+			hasLinearMovement = true;
 		}
 		if (Input.IsActionPressed("MoveBackward"))
 		{
-			Velocity -= new Vector2(delta * MoveSpeed, 0);
-			hasInput = true;
+			LocalVelocity -= new Vector2(delta * MoveSpeed, 0);
+			hasLinearMovement = true;
 		}
-		var length = Velocity.LengthSquared();
+		var length = LocalVelocity.LengthSquared();
 		if (length > (MoveSpeed * MoveSpeed))
 		{
-			var direction = Velocity.Normalized();
-			Velocity = direction * MoveSpeed;
+			var direction = LocalVelocity.Normalized();
+			LocalVelocity = direction * MoveSpeed;
 		}
-		if (!hasInput)
+		if (!hasLinearMovement)
 		{
-			Velocity = Velocity * Math.Max(1 - (SlowRate * delta), 0.0F);
+			LocalVelocity = LocalVelocity * Math.Max(1 - (SlowRate * delta), 0.0F);
 		}
-		Position += Velocity * delta;
+		Position += LocalVelocity.Rotated(Rotation) * delta;
+	}
+
+	private float LargestMagnitude(params float[] inputs)
+	{
+		if (inputs.Length == 0)
+		{
+			return 0.0F;
+		}
+		float? winner = null;
+		foreach (var input in inputs)
+		{
+			if (winner is null)
+			{
+				winner = input;
+			}
+			else if (Math.Abs(winner.Value) < Math.Abs(input))
+			{
+				winner = input;
+			}
+		}
+		return winner.Value;
+	}
+
+	private float SmallestMagnitude(params float[] inputs)
+	{
+		if (inputs.Length == 0)
+		{
+			return 0.0F;
+		}
+		float? winner = null;
+		foreach (var input in inputs)
+		{
+			if (winner is null)
+			{
+				winner = input;
+			}
+			else if (Math.Abs(winner.Value) > Math.Abs(input))
+			{
+				winner = input;
+			}
+		}
+		return winner.Value;
 	}
 }
