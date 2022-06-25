60db3aa38b8248b2411c7f0bada04d4546485f44
Brendan Lyon <brendan.lyon@claritymis.com>
2022-06-24 23:48:20 -0500

    Additional utilities

--- scripts/Utils.cs
+++ scripts/Utils.cs
@@ -2,25 +2,67 @@ using Godot;
 
 public static class Utils
 {
-	public static TNodeType GetChildNodeByName<TNodeType>(this Node parent, string name)
-		where TNodeType : Node
+    public static TNodeType GetChildNodeByName<TNodeType>(this Node parent, string name)
+        where TNodeType : Node
+    {
+        foreach (var child in parent.GetChildren())
+        {
+            if (!(child is Node childNode))
+            {
+                continue;
+            }
+            if (child is TNodeType childTNode && childTNode.Name == name)
+            {
+                return childTNode;
+            }
+            var namedChild = childNode.GetChildNodeByName<TNodeType>(name);
+            if (namedChild != null)
+            {
+                return namedChild;
+            }
+        }
+        return null;
+    }
+
+	public static float LargestMagnitude(params float[] inputs)
 	{
-		foreach (var child in parent.GetChildren())
+		if (inputs.Length == 0)
 		{
-			if (!(child is Node childNode))
+			return 0.0F;
+		}
+		float? winner = null;
+		foreach (var input in inputs)
+		{
+			if (winner is null)
 			{
-				continue;
+				winner = input;
 			}
-			if (child is TNodeType childTNode && childTNode.Name == name)
+			else if (Mathf.Abs(winner.Value) < Mathf.Abs(input))
 			{
-				return childTNode;
-			}
-			var namedChild = childNode.GetChildNodeByName<TNodeType>(name);
-			if (namedChild != null)
-			{
-				return namedChild;
+				winner = input;
 			}
 		}
-		return null;
+		return winner.Value;
+	}
+
+	public static float SmallestMagnitude(params float[] inputs)
+	{
+		if (inputs.Length == 0)
+		{
+			return 0.0F;
+		}
+		float? winner = null;
+		foreach (var input in inputs)
+		{
+			if (winner is null)
+			{
+				winner = input;
+			}
+			else if (Mathf.Abs(winner.Value) > Mathf.Abs(input))
+			{
+				winner = input;
+			}
+		}
+		return winner.Value;
 	}
 }
