4965a2f35ab3055315557ceb91915bd5fe0474ca
Brendan Lyon <brendan.lyon@claritymis.com>
2022-06-24 20:57:01 -0500

    Ship script progress

--- scripts/Ship.cs
+++ scripts/Ship.cs
@@ -1,21 +1,44 @@
 using Godot;
 using System;
 
-public class Ship : Node
+public class Ship : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Stats
+	public float MoveSpeed { get; set; }
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	// Velocity
+	public Vector2 Velocity { get; set; }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Velocity = Vector2.Zero;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		var hasInput = false;
+		if (Input.IsActionPressed("MoveForward"))
+		{
+			Velocity += new Vector2(delta * MoveSpeed, 0);
+			hasInput = true;
+		}
+		if (Input.IsActionPressed("MoveBackward"))
+		{
+			Velocity -= new Vector2(delta * MoveSpeed, 0);
+			hasInput = true;
+		}
+		var length = Velocity.LengthSquared();
+		if (length > (MoveSpeed * MoveSpeed))
+		{
+			var direction = Velocity.Normalized();
+			Velocity = direction * MoveSpeed;
+		}
+		if (!hasInput)
+		{
+			Velocity = Velocity * 0.75F * delta;
+		}
+		Position += Velocity * delta;
+	}
 }
